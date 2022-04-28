01d825385375d01600021c945513e34a405d0d27
Deviatov, Stanislav [External] <Stanislav.Deviatov@externals.adidas.com>
2022-04-28 10:04:33 +0300

    Create CamelSoapApplicationTests.java

---
+++ src/test/java/com/example/CamelSoap/CamelSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CamelSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
